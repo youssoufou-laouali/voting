15ebe68088eee2d90d767a3e327d95e08d9e9e0c
laouali baraou <youssoufoulaoualibaraou@gmail.com>
2020-10-09 19:47:00 +0100

    Updates

---
+++ precache-manifest.e53d2435b3c8549220ca30e98a2fa73f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d4c758674bc69ca0193b058f4f039b",
+    "url": "/voting/index.html"
+  },
+  {
+    "revision": "7d8082a11794af1fb366",
+    "url": "/voting/static/css/2.0c2b7c2d.chunk.css"
+  },
+  {
+    "revision": "c6cf12ef39ed797858c2",
+    "url": "/voting/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7d8082a11794af1fb366",
+    "url": "/voting/static/js/2.e971f3cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voting/static/js/2.e971f3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6cf12ef39ed797858c2",
+    "url": "/voting/static/js/main.9b9a94fd.chunk.js"
+  },
+  {
+    "revision": "a73667cf4c0f5a0128d3",
+    "url": "/voting/static/js/runtime-main.f0d70bed.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/voting/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/voting/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/voting/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/voting/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/voting/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
